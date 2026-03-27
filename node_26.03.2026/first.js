c88dc10223c2607c40fe8f3916d31b216e2722df
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-27 10:14:06 +0530

    first JavaScript file

---
+++ node_26.03.2026/first.js
@@ -0,0 +1,33 @@
+console.log("Welcome to Node JS");
+let x=20.5;
+console.log(x);
+let a;
+console.log(a);
+const y=20;
+console.log(y);
+//y=30;
+console.log(y);
+console.log(typeof x);
+let s="vignarathan";
+console.log(typeof s);
+let p;
+console.log(typeof p);
+let bool=true
+console.log(typeof bool);
+let data=null;
+console.log(typeof data);
+let arr=[];
+console.log(typeof arr);
+let obj={};
+console.log(typeof obj);
+let big=2564928483456436583926458632456n;
+console.log(typeof big);
+let id=Symbol("id");
+console.log(typeof id);
+let fn=function(){
+    console.log("hello");
+};
+console.log(typeof fn);
+
+
+

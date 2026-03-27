a8edd2497310fc6ba7ed3733670b0c484227fdbb
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-27 12:08:26 +0530

    relation operators and backtick

---
+++ node_27.03.2026/operators.js
@@ -0,0 +1,19 @@
+/*x=10;
+y="10";
+console.log(x==y); //== only check valus
+console.log(x===y); //=== check values and data types
+
+let myname="vikki";
+let myname1='vikki';
+let myname2=`vikki`;
+
+console.log(myname);
+console.log(myname1);
+console.log(myname2);*/
+
+let fname="vikki";
+let lname="rathan";
+let age=21;
+
+console.log(`my full name ${fname} ${lname} \nmy age ${age}`);
+console.log(`${12+3}`);

8ebad2ab8c704b0e0af0e78e5cb79819edbaeaa5
S.Vignarathan <s.vignarathan@gmail.com>
2026-04-21 13:21:08 +0530

    Update function1.js

--- Anonymous_node_09.04.2026/function1.js
+++ Anonymous_node_09.04.2026/function1.js
@@ -1,4 +1,9 @@
-myname();
+myid();
+function myid(){
+    console.log("my id is 25044");
+}
+
+//myname();     //can't call a anonymous function before declaring it
 let myname=function (){
 
     console.log("my name is vignarathan");
